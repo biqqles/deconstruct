ce567e8a7fd4e3c009474b9c0ef38a819f2577cb
biqqles <biqqles@protonmail.com>
2020-11-07 00:20:18 +0000

    Update setup.py for v0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='deconstruct',
-    version='0.5',
+    version='0.6',
     author='biqqles',
     author_email='biqqles@protonmail.com',
     description='Pythonic C-style structs for parsing binary data',
@@ -14,6 +14,7 @@ setup(
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
     ],
     python_requires='>=3.6',  # uses the type annotation syntax defined in PEP 526
 )
